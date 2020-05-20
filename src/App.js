af2ed4dd893d5e4c9dc4827bbc53852862fb511c
akozlova <annakozlova92@gmail.com>
2020-05-20 15:15:34 -0700

    attempt to fix issue with loading books

--- src/App.js
+++ src/App.js
@@ -5,27 +5,29 @@ import * as BooksAPI from './BooksAPI'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    // showSearchPage: false,
-    books: []
+    books: [],
+    loading: true
   }
   componentDidMount() {
     BooksAPI.getAll()
     .then((books) => {
-      this.setState({ books })
+      this.setState({ books },
+        this.setState({ loading: false }))
     })
+    .catch(error => console.log(error))
   }
-
   render() {
+    const { loading, books } = this.state
+    if (loading) {
+      return (
+      <div>
+        <p>Loading, please wait</p>
+      </div>)
+    };
     return (
       <div className="app">
-        <AllBooks books={this.state.books}/>
-        {console.log('BooksAPI contains: ' + this.books)}
+        <AllBooks books={books}/>
+        {console.log('BooksAPI contains: ' + books)}
       </div>
     )
   }
